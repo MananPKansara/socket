83a29e497d9d0a27ed30284a2a527484d9cd7d55
Manan Kansara <94055622+MananPKansara@users.noreply.github.com>
2023-03-24 17:10:32 +0530

    second commit

--- client.js
+++ client.js
@@ -1,18 +1,52 @@
 const socket = io("http://127.0.0.1:3000");
 
-let user = prompt('Enter new user');
+do{
+    user = prompt('Enter new user');
+}while(user==null)
 socket.emit('new-user-joined', user);
 
-socket.on('user-joined', function(name) {
+socket.on('my-self', (name) => {
+    let heading = document.querySelector('.heading');
+    heading.textContent = `Welcome ${name}`;
+})  
+
+socket.on('user-joined', (name) => {
+    // console.log(`event emited for ${name}`);
     let elem = document.createElement('div');
-    elem.id = "elem"
+    elem.classList.add('message', 'fs-4', 'text-center', 'my-5')
     elem.textContent =`${name} has joined the chat`;
 
-    document.body.appendChild(elem)
+    let messageContainer = document.querySelector('.message-container');
+    messageContainer.appendChild(elem);
 });
 
 socket.on('recieve', (data) => {
     console.log("Recieved");
+    let elem = document.createElement('div');
+    elem.classList.add('message', 'fs-4', 'text-left', 'my-5')
+    elem.textContent =`${data.name} : ${data.message}`;
+
+    let messageContainer = document.querySelector('.message-container');
+    messageContainer.appendChild(elem);
 });
 
-socket.emit('send', "Manan")
+socket.on('user-recieve', (data) => {
+    let elem = document.createElement('div');
+    elem.classList.add('message', 'fs-4', 'my-5')
+    elem.style.textAlign = 'right'
+    elem.textContent =`${data.message}`;
+
+    let messageContainer = document.querySelector('.message-container');
+    messageContainer.appendChild(elem);
+})
+
+socket.on('userChecok', (user) => {
+    console.log(user);
+})
+
+function sendMessage()
+{
+    let message = document.getElementById('message');
+    socket.emit('send', message.value);
+    message.value = "";
+}

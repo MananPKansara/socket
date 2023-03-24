9acf15ca2d63f483cf7fde3a40e6cbfded23ab18
Manan Kansara <94055622+MananPKansara@users.noreply.github.com>
2023-03-24 17:11:00 +0530

    second commit

--- server.js
+++ server.js
@@ -1,22 +1,34 @@
-const io = require('socket.io')(3000, {
-    cors:
-    {
-        origin: "*"
-    }
+const app = require('express')();
+const server = require('http').createServer(app);
+
+
+const io = require('socket.io')(server, {
+        cors:
+        {
+                origin: "*"
+        }
 });
 
 let users = {};
 
 io.on('connection', socket => {
-    socket.on('new-user-joined', name => {
-        console.log(`${name} joined chat`);
-        users[socket.id] = name;
-        socket.emit('user-joined', name); 
-    })
 
-    socket.on('send', message => {
-        socket.emit('recieve', {message:message, name:users[socket.id]})
-    })
-
-    console.log("Connnection establised");
+        socket.on('new-user-joined', name => { 
+                console.log(`${name} joined chat`);
+                users[socket.id] = name;
+                // socket.emit('userChecok', users)
+                socket.emit('my-self', name);
+                socket.broadcast.emit('user-joined', name);
+        })
+        
+        socket.on('send', message => {
+                socket.emit('user-recieve', {message: message});
+                socket.broadcast.emit('recieve', { message: message, name: users[socket.id] });
+        })
+        
+        console.log("Connnection establised");
 });
+
+console.log(users);
+
+server.listen(3000)
